dec9050d44357faa69d49a7e99b30cad680a0b4e
imaosazee4github <86365081+imaosazee4github@users.noreply.github.com>
2023-10-27 19:40:55 +0100

    updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import express from 'express';  
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
 
 
 app.listen(3001, () => {
